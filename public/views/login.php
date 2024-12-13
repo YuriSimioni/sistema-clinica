320580555e062a8468cd8b315ba5a555feac757d
YuriSimioni <yurirs.contato@gmail.com>
2024-12-13 11:38:01 -0300

    Tela de login pronta

---
+++ public/views/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/root.css">
+    <link rel="stylesheet" href="../css/index_style.css">
+</head>
+<body>
+    <form action="" method="post">
+        <p id="title">LOGIN</p>
+        <input type="text" name="" id="" placeholder="Login" class="input">
+        <input type="password" name="" id="" placeholder="Senha" class="input">
+        <input type="submit" value="Entrar" class="btn">
+    </form>
+</body>
+</html>
